fbb702392cb6604bdc4c828675716bc8841bfe5c
Senono <alekseyy-asselborn@rambler.ru>
2018-07-24 16:17:11 +0300

    auto format

--- game.js
+++ game.js
@@ -1,62 +1,63 @@
 'use strict';
 
 class Vector {
-    constructor(x = 0, y= 0) {
-        this.x = x;
-        this.y = y;
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+  
+  plus(vector) {
+    if (!(vector instanceof Vector)) {
+      throw new Error('Можно прибавлять к вектору только вектор типа Vector');
     }
-
-    plus(vector) {
-        if (!(vector instanceof Vector)) {
-            throw new Error('Можно прибавлять к вектору только вектор типа Vector');
-        }
-        return new Vector(this.x + vector.x, this.y + vector.y);
-    }
-
-    times(multiplier) {
-        return new Vector(this.x * multiplier, this.y * multiplier);
-    }
+    return new Vector(this.x + vector.x, this.y + vector.y);
+  }
+  
+  times(multiplier) {
+    return new Vector(this.x * multiplier, this.y * multiplier);
+  }
 }
 
 class Actor {
-    constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
-        if (!(pos instanceof Vector) || !(size instanceof Vector) || !(speed instanceof Vector)) {
-            throw new Error()
-        }
-        this.pos = pos;
-        this.size = size;
-        this.speed = speed;
+  constructor(pos = new Vector(0, 0), size = new Vector(1, 1), speed = new Vector(0, 0)) {
+    if (!(pos instanceof Vector) || !(size instanceof Vector) || !(speed instanceof Vector)) {
+      throw new Error()
     }
-
-    act() {}
-
-    get left() {
-        return this.pos.x;
+    this.pos = pos;
+    this.size = size;
+    this.speed = speed;
+  }
+  
+  act() {
+  }
+  
+  get left() {
+    return this.pos.x;
+  }
+  
+  get top() {
+    return this.pos.y;
+  }
+  
+  get right() {
+    return this.pos.x + this.size.x;
+  }
+  
+  get bottom() {
+    return this.pos.y + this.size.y;
+  }
+  
+  get type() {
+    return 'actor';
+  }
+  
+  isIntersect(actor) {
+    if (!(actor instanceof Actor) || arguments.length === 0) {
+      throw new Error();
     }
-
-    get top() {
-        return this.pos.y;
-    }
-
-    get right() {
-        return this.pos.x + this.size.x;
-    }
-
-    get bottom() {
-        return this.pos.y + this.size.y;
-    }
-
-    get type() {
-        return 'actor';
-    }
-
-    isIntersect(actor) {
-        if (!(actor instanceof Actor) || arguments.length === 0) {
-            throw new Error();
-        }
-
-        return !(actor === this || this.left >= actor.right || this.top >= actor.bottom || actor.left >= this.right || actor.top >= this.bottom);
-    }
+    
+    return !(actor === this || this.left >= actor.right || this.top >= actor.bottom || actor.left >= this.right || actor.top >= this.bottom);
+  }
 }
 
 class Level {
